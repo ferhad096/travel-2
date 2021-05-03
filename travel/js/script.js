ca3d12596fa6f02e145a31db5b6859fe813cbaf4
Ferhad Dashdizade <79448274+ferhad096@users.noreply.github.com>
2021-05-04 01:06:26 +0400

    Add files via upload

---
+++ travel/js/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+$('.discover').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    arrows: true
+  });
+
+AOS.init(1500);
